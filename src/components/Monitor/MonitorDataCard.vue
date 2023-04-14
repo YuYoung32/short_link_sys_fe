7b32c0fcd08ca9e9e19263e3269a1914af18e508
YuYoung <yuyoung32@qq.com>
2023-04-14 14:39:44 +0800

    fix(MonitorDataCard.vue): modify xl card height
    [dev-version-0.5.5]
    - Modify xl card height to 7rem.

--- src/components/Monitor/MonitorDataCard.vue
+++ src/components/Monitor/MonitorDataCard.vue
@@ -19,7 +19,7 @@
                 <span>30秒</span>
                 <span>0</span>
             </div>
-            <div class="flex justify-content-between h-auto xl:h-10rem">
+            <div class="flex justify-content-between h-auto xl:h-7rem">
                 <!--实时数据, 插入-->
                 <slot name="real-time-info" />
                 <!--静态数据, 插入-->
